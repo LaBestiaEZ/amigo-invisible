b18578cb6f23212422a5fe6e07768fdf519aecc6
Your Name <you@example.com>
2025-11-14 00:54:24 +0100

    Limpiar manifest.json y actualizar main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/service-worker.js')
       .then(registration => {
-        console.log('✅ Service Worker registrado:', registration.scope)
+        //console.log('✅ Service Worker registrado:', registration.scope)
       })
       .catch(error => {
         console.log('❌ Error al registrar Service Worker:', error)
